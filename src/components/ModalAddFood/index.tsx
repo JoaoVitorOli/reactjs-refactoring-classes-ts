2054e03e7bf63359006b2b64dbcfd297179f59c9
João Vitor <infamoz_br3@hotmail.com>
2021-09-07 17:36:30 -0300

    :godmode: desafio concluido

---
+++ src/components/ModalAddFood/index.tsx
@@ -0,0 +1,49 @@
+import { useRef } from 'react';
+import { FiCheckSquare } from 'react-icons/fi';
+
+import { FoodsTypes } from "../../pages/Dashboard";
+
+import { Form } from './styles';
+import Modal from '../Modal';
+import Input from '../Input';
+
+interface ModalAddFoodProps {
+  setIsOpen: () => void;
+  handleAddFood: (food: FoodsTypes) => Promise<void>;
+  isOpen: boolean;
+}
+
+function ModalAddFood({
+  setIsOpen,
+  handleAddFood,
+  isOpen,
+}: ModalAddFoodProps) {
+  const formRef = useRef(null);
+
+  async function handleSubmit(data: FoodsTypes) {
+    handleAddFood(data);
+    setIsOpen();
+  };
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <h1>Novo Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+        <button type="submit" data-testid="add-food-button">
+          <p className="text">Adicionar Prato</p>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+};
+
+export default ModalAddFood;
